91904435e7959967a0e7aaa789021f75071ac842
Dmitry <114305348+dmitrypopof@users.noreply.github.com>
2025-03-03 21:26:54 +0300

    Update Read_command.java

--- src/main/java/Commands_database/Read_command.java
+++ src/main/java/Commands_database/Read_command.java
@@ -17,7 +17,7 @@ public class Read_command {
 
         while(rs.next()) // извлечь всю таблицу (Лимит извлечения строк ставить в SQL-запросе переменной query "LIMIT")
 
-        {   // собираем в массив (не забудь импорт библа для массивов), указывая столбцы в rs.getString
+        {   // собираем в массив, указывая столбцы в rs.getString
             String [] MyQuery= {rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),rs.getString(6)} ;
             System.out.println(Arrays.toString(MyQuery));
         }
